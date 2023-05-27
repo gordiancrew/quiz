432e8effc1252659c4f0f8d7a67a9dc913110502
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-05-27 22:51:56 +0300

    init project

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text><h1>We are START quiz!</h1></Text>
+      <Button onPress={() => alert("HELLO ANDRIK")} title="press me" />
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
