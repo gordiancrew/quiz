472fcd239e38e565ec124d97a7508bd9cfc082f0
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-06-04 01:35:49 +0300

    feat: create button and logo

--- App.js
+++ App.js
@@ -1,22 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text><h1>We are START quiz!</h1></Text>
-      <Button onPress={() => alert("HELLO ANDRIK")} title="press me" />
-     
-      <StatusBar style="auto" />
+      <Image style={styles.logo} source={require("./assets/logo.png")} />
+      
+      <TouchableOpacity
+        style={styles.but}
+        onPress={() => Alert.alert("Simple Button pressed")}
+      >
+        <Text style={styles.butTextStyle}>ИГРАТЬ</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.but}
+        onPress={() => Alert.alert("Simple Button pressed")}
+      >
+        <Text style={styles.butTextStyle}>ПРАВИЛА</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.but}
+        onPress={() => Alert.alert("Simple Button pressed")}
+      >
+        <Text style={styles.butTextStyle}>СОЗДАТЕЛИ</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 100,
+    paddingBottom: 100,
+    marginTop: 50,
+    marginBottom: 50,
+
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#7c422e",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  but: {
+    flex: 0.2,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#401506",
+    color: "white",
+
+    width: "80%",
+    height: 30,
+   
+    borderRadius: 10,
+  },
+  butTextStyle: {
+    color: "#e3e485",
+    fontSize: 20,
+  },
+  logo: {
+    width: "80%",
+    height: "15%",
   },
 });
