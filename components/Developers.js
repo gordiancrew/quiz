22533f19de72429fd4d78aca4b446825a0d3b864
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-06-13 00:49:07 +0300

    feat: add points

--- components/Developers.js
+++ components/Developers.js
@@ -1,19 +1,34 @@
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function Developers({ navigation }) {
-  const data = ["1", "2", "3", "4", "5"];
+  const data = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const count = 3;
   return (
     <View style={styles.container}>
       <FlatList
         style={styles.list}
+        numColumns={3}
+        // snapToAlignment="sspace-around"
+        // style={styles.list}
+        contentContainerStyle={{
+          backgroundColor: "blue",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
         data={data}
-        keyExtractor={(item, index) => item?.index}
+        // keyExtractor={(item, index) => item?.index}
         renderItem={({ item, index }) => (
           <View
             style={[
               styles.point,
-              count > index ? { backgroundColor: "black" } :count===index?{ backgroundColor: "yellow" }: {},
+              count > index
+                ? { backgroundColor: "black" }
+                : count === index
+                ? {
+                    backgroundColor: "yellow",
+                    shadowColor: "#171717",
+                  }
+                : {},
             ]}
           >
             <Text style={styles.pointTextStyle}>{item} </Text>
@@ -56,19 +71,22 @@ const styles = StyleSheet.create({
     height: "15%",
   },
   point: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    margin: 20,
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    margin: "5%",
     backgroundColor: "red",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     color: "white",
+    shadowColor: "red",
+    
   },
   list: {
-    width: 200,
-    backgroundColor: "green",
+    // backgroundColor: "green",
+    width: "100%",
+    
   },
   pointTextStyle: {
     color: "white",
